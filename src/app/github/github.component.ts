df267b04ea89b779c393bdcb45bdff969f766cc3
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-05-26 14:41:33 +0300

    update and modify the files

--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { GithubServiceService } from '../github-service.service';
+import { Repo } from '../repo';
+import { User } from '../user';
 
 @Component({
   selector: 'app-github',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./github.component.css']
 })
 export class GithubComponent implements OnInit {
+  user!: User
+  repoDetails!: Repo
+  githubServiceService!:GithubServiceService
 
-  constructor() { }
+  constructor( githubServiceService:GithubServiceService) {
+    this.githubServiceService=githubServiceService
+   }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    this.user = this.githubServiceService.user
+    this.repoDetails = this.githubServiceService.respositoryData
   }
 
 }
