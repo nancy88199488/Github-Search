613fe452c98dd45b362db3578fe60f398cfc85e5
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-05-26 22:32:42 +0300

    update the changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { UserprofileComponent } from './userprofile/userprofile.component';
 import { RepositoriesComponent }from './repositories/repositories.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     RepositoriesComponent,
     UserprofileComponent,
-    NavbarComponent
+    NavbarComponent,
+    
   ],
   imports: [
     BrowserModule,
