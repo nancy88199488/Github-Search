686d9115d284310ca07305b04ab00994f4d2e8ce
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-05-26 11:31:32 +0300

    update changse

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GithubFormComponent } from './github-form/github-form.component';
+import { GithubComponent } from './github/github.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"github-form",component:GithubFormComponent},
+  {path:"github", component:GithubComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
