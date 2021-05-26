5486b89d56a416ea608dcf00717ecd37f2a8c995
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-05-26 11:22:35 +0300

    initiate the form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubComponent } from './github/github.component';
+import { GithubFormComponent } from './github-form/github-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GithubComponent
+    GithubComponent,
+    GithubFormComponent
   ],
   imports: [
     BrowserModule,
