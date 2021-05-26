5890069a8c773d75e2acb330a6997ca0e2a63b08
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-05-26 13:22:04 +0300

    update and modify changes

--- src/app/github-service.service.ts
+++ src/app/github-service.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Repo } from './repo';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubServiceService {
-
-  constructor() { }
+  user!: User;
+  repo!: Repo;
+  newUserData:any=[]
+  respositoryData:any=[]
+  constructor(private http:HttpClient) { 
+    this.user = new User("","")
+    this.repo = new Repo("",new Date(),"","")
+  }
+  getUserData(user:string){
+    interface apiResponse
+  }
 }
+
